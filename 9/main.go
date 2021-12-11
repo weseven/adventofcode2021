276b517115f8b09974d1f2ed9257b862774e68a1
Daniel Simionato <weseven@gmail.com>
2021-12-11 12:30:24 +0100

    Refactored day9 to be more readable/extensible

--- 9/main.go
+++ 9/main.go
@@ -10,6 +10,11 @@ import (
 	"strconv"
 )
 
+type Point struct {
+	x int
+	y int
+}
+
 func main() {
 
 	inputfile, err := os.Open("input")
@@ -19,17 +24,19 @@ func main() {
 	}
 	defer inputfile.Close()
 
-	heightmap := make([]int, 0)
+	heightmap := make([][]int, 0)
 
 	scanner := bufio.NewScanner(inputfile)
 	numRows, numCols := 0, 0
 	for scanner.Scan() {
 		heightsRE := regexp.MustCompile(`\d`)
 		heights := heightsRE.FindAllString(scanner.Text(), -1)
+		var row []int
 		for _, str := range heights {
 			height, _ := strconv.Atoi(str)
-			heightmap = append(heightmap, height)
+			row = append(row, height)
 		}
+		heightmap = append(heightmap, row)
 		if numCols == 0 {
 			numCols = len(heights)
 		}
@@ -42,92 +49,62 @@ func main() {
 		log.Fatal(err)
 	}
 
-	lowPoints := make([]int, 0)
+	lowPoints := make([]Point, 0)
 	// Puzzle one
 	sumRisk := 0
-	for i, h := range heightmap {
-		if isLowPoint(heightmap, numRows, numCols, i) {
-			lowPoints = append(lowPoints, i)
-			sumRisk += (h + 1)
+	for i, row := range heightmap {
+		for j, h := range row {
+			if isLowPoint(heightmap, numRows, numCols, Point{x:j,y:i}) {
+				lowPoints = append(lowPoints, Point{x:j,y:i})
+				sumRisk += (h + 1)
+			}
 		}
 	}
-
 	fmt.Printf("Puzzle one sum risk: %d\n", sumRisk)
 
 	// Puzzle two
 	basinSizes := make([]int, len(lowPoints))
-	explored := make([]bool, len(heightmap))
+	explored := make(map[Point]bool)
 	for i, lowPoint := range lowPoints {
 		basinSizes[i] = exploreBasins(heightmap, lowPoint, &explored, numRows, numCols)
 	}
-	// fmt.Printf("BasinSizes: %v\n", basinSizes)
 	sort.Sort(sort.Reverse(sort.IntSlice(basinSizes)))
-	fmt.Printf("Puzzle 2 top 3 basinSizes product: %d\n",basinSizes[0]*basinSizes[1]*basinSizes[2])
-
+	fmt.Printf("Puzzle 2 top 3 basinSizes product: %d\n", basinSizes[0]*basinSizes[1]*basinSizes[2])
 }
 
-func exploreBasins(heightmap []int, pos int, explored *[]bool, rows int, cols int) (basinSize int) {
+func exploreBasins(heightmap [][]int, pos Point, explored *map[Point]bool, rows int, cols int) (basinSize int) {
 	(*explored)[pos] = true
-	if (pos - cols) >= 0 {
-		if heightmap[pos-cols] != 9 && !(*explored)[pos-cols] {
-			//explore top
-			basinSize += exploreBasins(heightmap, pos-cols, explored, rows, cols)
-		}
-	}
-	if (pos/cols) < (rows-1) && !(*explored)[pos+cols] {
-		if heightmap[pos+cols] != 9 {
-			//explore bottom
-			basinSize += exploreBasins(heightmap, pos+cols, explored, rows, cols)
-		}
-	}
-	if (pos%cols) != 0 && !(*explored)[pos-1] {
-		if heightmap[pos-1] != 9 {
-			//explore left
-			basinSize += exploreBasins(heightmap, pos-1, explored, rows, cols)
-		}
-	}
-	if (pos%cols) != (cols-1) && !(*explored)[pos+1] {
-		if heightmap[pos+1] != 9 {
-			//explore right
-			basinSize += exploreBasins(heightmap, pos+1, explored, rows, cols)
-		}
-	}
 	basinSize += 1
-
+	neighbourPoints := getNeighbours(rows,cols,pos)
+	for _, neighbourPoint := range neighbourPoints{
+		if heightmap[neighbourPoint.y][neighbourPoint.x]!=9 && !(*explored)[neighbourPoint]{
+			basinSize+=exploreBasins(heightmap,neighbourPoint,explored,rows,cols)
+		}
+	}
 	return basinSize
 }
 
-func isLowPoint(heightmap []int, rows int, cols int, pos int) bool {
-	//angles
-	if pos == 0 {
-		return ((heightmap[pos] < heightmap[pos+1]) && (heightmap[pos] < heightmap[pos+cols]))
+func getNeighbours(rows int, cols int, pos Point) (neighbours []Point) {
+	if pos.x > 0 {
+		neighbours = append(neighbours, Point{y:pos.y, x: pos.x-1})
 	}
-	if pos == (cols - 1) {
-		return ((heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos+cols]))
+	if pos.y > 0 {
+		neighbours = append(neighbours, Point{y:pos.y-1, x: pos.x})
 	}
-	if pos == (rows-1)*(cols) {
-		return ((heightmap[pos] < heightmap[pos+1]) && (heightmap[pos] < heightmap[pos-cols]))
+	if (pos.y + 1) < rows {
+		neighbours = append(neighbours, Point{y:pos.y+1, x: pos.x})
 	}
-	if pos == ((rows * cols) - 1) {
-		return ((heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos-cols]))
+	if (pos.x + 1) < cols {
+		neighbours = append(neighbours, Point{y:pos.y, x: pos.x+1})
 	}
-	if pos%cols == 0 {
-		//first column
-		return ((heightmap[pos] < heightmap[pos-cols]) && (heightmap[pos] < heightmap[pos+1]) && (heightmap[pos] < heightmap[pos+cols]))
-	}
-	if pos/cols == 0 {
-		//first row
-		return ((heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos+cols]) && (heightmap[pos] < heightmap[pos+1]))
-	}
-	if pos%cols == (cols - 1) {
-		//last column
-		return ((heightmap[pos] < heightmap[pos-cols]) && (heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos+cols]))
-	}
-	if pos/cols == (rows - 1) {
-		//last row
-		return ((heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos-cols]) && (heightmap[pos] < heightmap[pos+1]))
-	}
+	return neighbours
+}
 
-	return ((heightmap[pos] < heightmap[pos-1]) && (heightmap[pos] < heightmap[pos-cols]) && (heightmap[pos] < heightmap[pos+1]) && (heightmap[pos] < heightmap[pos+cols]))
-
+func isLowPoint(heightmap [][]int, rows int, cols int, pos Point) bool {
+	for _, neighbourPoint := range getNeighbours(rows, cols, pos) {
+		if !(heightmap[pos.y][pos.x] < heightmap[neighbourPoint.y][neighbourPoint.x]) {
+			return false
+		}
+	}
+	return true
 }
